d8423baa7dd0efedb4b13dbbe042a77d8e2535b5
Felipe Cortez Muniz <30799412+felipter@users.noreply.github.com>
2024-09-09 15:29:00 -0300

    Atualização com tags e ids de cada filme

--- dados.js
+++ dados.js
@@ -1,101 +1,231 @@
 let dados = [
     {
+        id: 1,
         titulo: "Harakiri",
         ano: 1962,
         duracao: 103,
         genero: "Drama, Period",
         descricao: "A disgraced samurai petitions his lord for permission to commit seppuku (ritual suicide). However, his motives and past are shrouded in mystery.",
         idioma: "Japanese",
-        link: "https://en.wikipedia.org/wiki/Harakiri_(1962_film)"
+        link: "https://en.wikipedia.org/wiki/Harakiri_(1962_film)",
+        tags: [
+            "samurai",
+            "seppuku",
+            "honor",
+            "bushido",
+            "revenge",
+            "period",
+            "social",
+            "psychological",
+            "japanese",
+            "black and white",
+          ],
     },
     
     {
+        id: 2,
         titulo: "Come and See",
         ano: 1985,
         duracao: 142,
         genero: "War, Drama",
         descricao: "A haunting portrayal of World War II through the eyes of a young Belarusian boy who becomes separated from his family and joins the Soviet resistance.",
         idioma: "Russian",
-        link: "https://en.wikipedia.org/wiki/Come_and_See"
+        link: "https://en.wikipedia.org/wiki/Come_and_See",
+        tags: [
+            "world war ii",
+            "belarus",
+            "soviet",
+            "soldier",
+            "innocence",
+            "anti-war",
+            "psychological",
+            "eastern",
+            "handheld",
+            "historical",
+          ],
     },
 
     {
+        id: 3,
         titulo: "12 Angry Men",
         ano: 1957,
         duracao: 96,
         genero: "Drama, Crime",
         descricao: "A jury of twelve deliberates the guilt or innocence of a young man accused of murdering his father. As they debate, hidden biases and deep-seated prejudices begin to surface.",
         idioma: "English",
-        link: "https://en.wikipedia.org/wiki/12_Angry_Men_(1957_film)"
+        link: "https://en.wikipedia.org/wiki/12_Angry_Men_(1957_film)",
+        tags: [
+            "jury",
+            "deliberation",
+            "justice",
+            "prejudice",
+            "racism",
+            "suspense",
+            "character study",
+            "american",
+            "black and white",
+            "classic",
+          ],
     },
 
     {
+        id: 4,
         titulo: "Seven Samurai",
         ano: 1954,
         duracao: 207,
         genero: "Drama, Action",
         descricao: "A poor village under constant threat of bandits hires seven unemployed samurai to protect them during the harvest season. The samurai must plan their defense and fight together as a unit.",
         idioma: "Japanese",
-        link: "https://en.wikipedia.org/wiki/Seven_Samurai"
+        link: "https://en.wikipedia.org/wiki/Seven_Samurai",
+        tags: [
+            "samurai",
+            "warfare",
+            "honor",
+            "community",
+            "social",
+            "action",
+            "adventure",
+            "japanese",
+            "epic",
+            "influential",
+          ],
     },
 
     {
+        id: 5,
         titulo: "The Godfather: Part II",
         ano: 1974,
         duracao: 200,
         genero: "Crime, Drama",
         descricao: "The story of the Corleone family continues as Michael attempts to solidify his power as the head of a mafia clan. The film also explores the life of young Vito Corleone and his rise to power.",
         idioma: "English",
-        link: "https://en.wikipedia.org/wiki/The_Godfather_Part_II"
+        link: "https://en.wikipedia.org/wiki/The_Godfather_Part_II",
+        tags: [
+            "mafia",
+            "crime",
+            "family",
+            "power",
+            "corruption",
+            "sequel",
+            "gangster",
+            "american",
+            "epic",
+            "critical acclaim",
+        ],
     },
 
     {
+        id: 6,
         titulo: "High and Low",
         ano: 1963,
         duracao: 130,
         genero: "Crime, Drama",
         descricao: "A wealthy businessman's son is kidnapped, and a ransom is demanded. As the police investigate and the family negotiates, the lines between truth and deception blur.",
         idioma: "Japanese",
-        link: "https://en.wikipedia.org/wiki/High_and_Low_(film)"
+        link: "https://en.wikipedia.org/wiki/High_and_Low_(film)",
+        tags: [
+            "suspense", 
+            "crime",
+            "drama",
+            "social",
+            "struggle",
+            "kidnapping",
+            "dilemma",
+            "japanese",
+            "mystery",
+            "black and white"
+        ]
     },
 
     {
+        id: 7,
         titulo: "The Human Condition III: A Soldier's Prayer",
         ano: 1961,
         duracao: 237,
         genero: "War, Drama",
         descricao: "The final chapter in the epic trilogy follows Kaji as he faces the brutality of a Manchurian forced labor camp and grapples with his humanity in the face of unimaginable suffering.",
         idioma: "Japanese",
-        link: "https://en.wikipedia.org/wiki/The_Human_Condition_(film_series)"
+        link: "https://en.wikipedia.org/wiki/The_Human_Condition_(film_series)",
+        tags: [
+            "war",
+            "japanese",
+            "historical",
+            "world war ii",
+            "prison",
+            "humanity",
+            "survival",
+            "labor",
+            "epic",
+            "psychological"
+        ]
     },
 
 
-        {
+    {
+        id: 8,
         titulo: "Shawshank Redemption",
         ano: 1994,
         duracao: 142,
         genero: "Crime, Drama",
         descricao: "Imprisoned in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates – including an older prisoner named Red – for his integrity and unquenchable sense of hope.",
         idioma: "English",
-        link: "https://en.wikipedia.org/wiki/The_Shawshank_Redemption"
+        link: "https://en.wikipedia.org/wiki/The_Shawshank_Redemption",
+        tags: [
+            "prison",
+            "escape",
+            "hope",
+            "friendship",
+            "injustice",
+            "redemption",
+            "drama",
+            "psychological",
+            "truestory",
+            "classic"
+            ]
     },
     {
+        id: 9,
         titulo: "Parasite",
         ano: 2019,
         duracao: 133,
         genero: "Comedy, Thriller, Drama",
         descricao: "All unemployed, Ki-taek’s family takes peculiar interest in the wealthy and glamorous Parks for their livelihood until they get entangled in an unexpected incident.",
         idioma: "Korean",
-        link: "https://en.wikipedia.org/wiki/Parasite_(2019_film)"
+        link: "https://en.wikipedia.org/wiki/Parasite_(2019_film)",
+        tags: [
+            "struggle",
+            "social",
+            "thriller",
+            "drama",
+            "family",
+            "Korea",
+            "darkcomedy",
+            "suspense",
+            "poverty",
+            "wealth"
+        ]
     },
     {
+        id: 10,
         titulo: "The Godfather",
         ano: 1972,
         duracao: 177,
         genero: "Crime, Drama",
         descricao: "Set between 1945 and 1955, this is a chronicle of the fictional Italian-American Corleone crime family. When the patriarch of the organized crime family, Vito Corleone, barely survives an assassination attempt, his youngest son, Michael, steps in to take care of those responsible, launching a bloody vendetta.",
         idioma: "English",
-        link: "https://pt.wikipedia.org/wiki/The_Godfather"
+        link: "https://pt.wikipedia.org/wiki/The_Godfather",
+        tags: [
+            "mafia",
+            "crime",
+            "family",
+            "power",
+            "corruption",
+            "gangster",
+            "italian",
+            "drama",
+            "classic",
+            "cult"
+        ]
     }
 
 ];
